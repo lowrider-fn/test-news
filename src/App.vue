85cf223b741fd7ae1da32608c3e459b7523eef55
lowrider-fn <lowrider-fn@ozon.ru>
2020-03-14 22:49:25 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
     <div id="app">
-        <Header :isAuth="isAuth"
-                @logout="logoutHandler()"
-        />
         <main>
             <router-view />
         </main>
@@ -11,27 +8,12 @@
 
 <script>
 
-import Header from '@/components/blocks/Header';
 import { mapGetters, mapActions } from 'vuex';
 
 export default {
     name      : 'App',
     components: {
-        Header,
     },
-    computed: {
-        ...mapGetters([
-            'isAuth',
-        ]),
-    },
-    methods: {
-        ...mapActions([
-            'logout',
-        ]),
-        async logoutHandler() {
-            await this.logout();
-            this.$router.push({ name: 'Auth' });
-        },
-    },
+
 };
 </script>
